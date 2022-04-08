dad4be0f5d0ba6833dd3b1e044fc894a840b524e
rambus06 <w2227@e-mirim.hs.kr>
2022-04-08 09:57:32 +0900

    20220408까지 한 것들

---
+++ Textual.java
@@ -0,0 +1,14 @@
+public class Textual {
+	public static void main( String args[]) {
+		/*char a,b,c;
+		a= 'A';
+		b='วั ';
+	System.out.println( a );
+	System.out.println( b );*/
+
+ 	int a =3;
+	int b =5;
+	System.put.println(a+b);
+
+	}
+}
---
+++ main2.java
@@ -0,0 +1,14 @@
+public class main2{
+	public static void main(Strong args[]){
+		int a= 19;
+		int b=2147483647;
+		double c=3.14;
+		double d =7.783;
+	
+		int e= a+b;
+		double f=a+c;		
+		double g= c+d;
+		double h=c+d;
+		
+	}
+}
