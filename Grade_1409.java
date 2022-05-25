4cc07886ca8b32760683ba6416dda9af76f41048
rambus06 <w2227@e-mirim.hs.kr>
2022-05-25 22:38:24 +0900

    000_grade
    파일정리

---
+++ Grade_1409.java
@@ -0,0 +1,34 @@
+
+public class Grade_1409{
+   public static void main(String args[]){
+
+
+
+	  System.out.println("----------자바실행----------");
+              System.out.println("이름    과제    보너스   합계");
+              System.out.print(name+"\t"+sub+"\t"+bonus+"\t"+tot);
+
+	  String name= 선미;
+	  int project=
+     	 System.out.print(name+" "+sub+" "+bonus+" jav"+tot);
+     	 System.out.printf("%s\t%d%d%d",name,project,bonus,tot);
+   
+     	 System.out.print("이름을 입력하세요 : ");
+      	String name= scan.next();
+     	 System.out.println("\t");
+
+      System.out.print("과제 점수를 입력하세요 : ");
+      int project3= scan.nextInt();
+      System.out.println("\t");
+
+      System.out.print("보너스 점수를 입력하세요 : ");
+      int bonus3= scan.nextInt();
+      System.out.println("\t");
+
+      int c=project3+bonus3;
+
+    
+      
+   }
+
+}
