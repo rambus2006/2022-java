633e4771e776d0138ccfdd8dfe091b8b739fc776
rambus06 <w2227@e-mirim.hs.kr>
2022-05-27 23:26:40 +0900

    003_데이터형&변수
    상수는 변하지 않는 final값이고, 변수는 입력 받을 때마다 덮어 씌워지는 값인 것을 알게 되었다.

--- Textual.java
+++ Textual.java
@@ -1,14 +1,15 @@
 public class Textual {
 	public static void main( String args[]) {
-		/*char a,b,c;
-		a= 'A';
-		b='한 ';
+		char a,b,c;
+		 a= 'A';
+		 b='한';
+		c='\u0061';	//유니코드 a
 	System.out.println( a );
-	System.out.println( b );*/
-
- 	int a =3;
+	System.out.println( b );
+	System.out.println(  );
+ 	/* int a =3;
 	int b =5;
-	System.put.println(a+b);
+	System.out.println(a+b);*/
 
 	}
 }
