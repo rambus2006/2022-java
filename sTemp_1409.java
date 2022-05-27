1a0c0e4e5ac6bad3c9c1524cfe03c942ca688d0e
rambus06 <w2227@e-mirim.hs.kr>
2022-05-27 23:29:38 +0900

    007_입력문

--- sTemp_1409.java
+++ sTemp_1409.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class sTemp_1409{
 	public static void main ( String args[]){
 		Scanner scan = new Scanner(System.in);
+
+		//입력받아서 온도 구하기
 		System.out.print("섭씨 온도 입력 : ");
 		int c = scan.nextInt( ) ;		//프로그래밍 하기 전 그전단계까지 실행되는지 꼭 확인하기!
 		double F =(double)9.0/5*c+32 ;	//정수와 정수 계산은 정수로 인식하므로 9랑 5중에 하나를 실수로 바꾸거나 둘 다 실수형으로 바꾸면 된다.
